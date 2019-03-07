a9370a87536b2d3142a3ddb652c8dac68c34e244
dkozak <dkozak94@gmail.com>
2019-03-07 16:32:02 +0000

    use environment for baseurl and make first production build

--- frontend/src/app/_service/task.service.ts
+++ frontend/src/app/_service/task.service.ts
@@ -2,18 +2,17 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {GofyczTask} from "../model/GofyczTask";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  private baseUrl: string = "https://gofycz.herokuapp.com";
-
   constructor(private http: HttpClient) {
   }
 
   getRandomTask(): Observable<GofyczTask> {
-    return this.http.get<GofyczTask>(`${this.baseUrl}/api/random-task`)
+    return this.http.get<GofyczTask>(`${environment.baseUrl}/api/random-task`)
   }
 }
